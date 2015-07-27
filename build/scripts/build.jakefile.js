794180211dd170f67297f9b4bf35e5aac2eed77f
j-funk <jaboing@hotmail.com>
2015-07-27 03:39:38 -0400

    removed the spike directory since we're working with large amounts of 3rd party code

--- build/scripts/build.jakefile.js
+++ build/scripts/build.jakefile.js
@@ -32,7 +32,7 @@
     task("lintClient", function() {
         process.stdout.write("Linting browser code:");
         jshint.checkFiles({
-            files: ["src/client/**/*.js", "spikes/**/*.js"],
+            files: ["src/client/**/*.js"],
             options: jshintConfig.clientOptions,
             globals: jshintConfig.clientGlobals
         }, complete, fail);
