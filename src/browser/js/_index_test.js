1c8192b2bfa1e308c678a62825ce670c24a47bab
j-funk <jaboing@hotmail.com>
2015-07-31 14:45:40 -0400

    node installed tape for testing browser code

--- src/browser/js/_index_test.js
+++ src/browser/js/_index_test.js
@@ -1,12 +1,16 @@
 /**
  * Created by jaboing on 2015-07-31.
  */
-//var assert = require("assert")
-//describe('Array', function() {
-//    describe('#indexOf()', function () {
-//        it('should return -1 when the value is not present', function () {
-//            assert.equal(-1, [1,2,3].indexOf(5));
-//            assert.equal(-1, [1,2,3].indexOf(0));
-//        });
-//    });
-//});
+"use strict";
+
+var assert = require("assert");
+var 
+
+describe('Array', function() {
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
+            assert.equal(-1, [1,2,3].indexOf(5));
+            assert.equal(-1, [1,2,3].indexOf(0));
+        });
+    });
+});
