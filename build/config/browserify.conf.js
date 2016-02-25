9e5a525604b4ef2f7cccb88c74d98ef285503bd5
j-funk <jaboing@hotmail.com>
2016-02-24 22:18:11 -0500

    copied paper spike into src/client

--- build/config/browserify.conf.js
+++ build/config/browserify.conf.js
@@ -6,12 +6,15 @@
 	"use strict";
 
 	exports.pathList = [
+		// spike bundles
         './spikes/paper/js/lesson.js -o ./spikes/paper/js/lesson.bundle.js',
 		'./spikes/practice_tuner/js/tuner.js -o ./spikes/practice_tuner/js/tuner.bundle.js',
 		'./spikes/canvas/scripts/canvas.js -o ./spikes/canvas/scripts/canvas.bundle.js',
 		'./spikes/pitch/js/getBuffersFromTones.js -o ./spikes/pitch/js/getBuffersFromTones.bundle.js',
 		'./spikes/pitch/js/drive_mpm.js -o ./spikes/pitch/js/drive_mpm.bundle.js',
-		'./spikes/pitch/js/drive_webAudio.js -o ./spikes/pitch/js/drive_webAudio.bundle.js'
+		'./spikes/pitch/js/drive_webAudio.js -o ./spikes/pitch/js/drive_webAudio.bundle.js',
+		// the real deal, not spikes
+		'./src/client/js/main.js -o ./src/client/js/main.bundle.js'
 	];
 
 
