a6c6871d62660184a382f341c8809aafc63784a9
j-funk <jaboing@hotmail.com>
2016-05-01 02:10:56 -0400

    back to normal phantomjs launcher

--- gruntfile.js
+++ gruntfile.js
@@ -25,7 +25,7 @@ module.exports = function(grunt) {
                 reporters: ['progress', 'coverage'],
                 autoWatch: false,
                 singleRun: true,
-                browsers: ['PhantomJS2'],
+                browsers: ['PhantomJS'],
                 browserify: {
                     debug: true,
                     transform: [
