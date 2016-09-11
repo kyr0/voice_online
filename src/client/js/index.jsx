b4d2d25ebbaa9dd855d98ebffcdb6d15fd5df493
j-funk <jaboing@hotmail.com>
2016-09-10 22:17:44 -0400

    cleanup index.jsx

--- src/client/js/index.jsx
+++ src/client/js/index.jsx
@@ -1,7 +1,6 @@
 'use strict';
 
 import 'babel-polyfill';
-// var $ = require('jquery');
 
 import React from 'react';
 import { render } from 'react-dom';
@@ -27,46 +26,3 @@ render((
         </Router>
     </Provider>
 ), document.getElementById('content'));
-
-
-// var Canvas = require('./Canvas.js');
-// var Audio = require('./Audio.js');
-//
-// var canvas = new Canvas();
-// var audio = null;
-//
-// // these window assignments must be done outside of onLoad
-// // for sharing with paper.js in case they are accessed before load
-// window.percentComplete = 0;
-// window.pitchFreq = -1;
-// window.lPlayer = null;
-// window.curLesson = null;
-//
-
-// render(
-//     <Router history={hashHistory}>
-//         <Route path="/" component={Main}>
-//             <Route path="/sing" component={Sing}/>
-//             <Route path="/profile" component={Profile}/>
-//         </Route>
-//     </Router>,
-//     document.getElementById('react-content')
-// );
-
-
-// // canvas onClick, start the lesson
-// $('#lesson').click(function(){
-//     console.log("JQUERY CLICK");
-//     canvas.initLesson();
-//     if (window.lPlayer) {
-//         audio.resetAudio(audio.getSingleNoteTestInput, window.lPlayer);
-//         // audio.resetAudio(audio.getUserInput);
-//         // audio.resetAudio(audio.getTestInput);
-//         window.lPlayer.start();
-//     }
-// });
-
-
-// $(document).ready(function() {
-//     audio = new Audio();  // AudioContext needs DOM (I think)
-// });
