13b317b6ee68113de121cae91c46f07e400110f0
j-funk <jaboing@hotmail.com>
2016-12-11 00:10:33 -0500

    Prevent get/update user request on anon user

--- src/client/js/index.jsx
+++ src/client/js/index.jsx
@@ -6,7 +6,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import { Provider } from 'react-redux';
-import Custom from '../custom.less';
+import Custom from '../assets/custom.less';
 
 import configureStore from './react/store/configureStore.babel';
 import App from './react/containers/App.babel';
