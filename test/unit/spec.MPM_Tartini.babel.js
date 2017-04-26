922005fd44a952f7e6f153cff0f02158d9da2fb3
j-funk <jaboing@hotmail.com>
2017-04-25 22:41:21 -0400

    start on tests for Tartini port

---
+++ test/unit/spec.MPM_Tartini.babel.js
@@ -0,0 +1,15 @@
+import calculateAnalysisData from '../../src/client/js/MPM_Tartini.babel.js';
+import buffers from '../fixtures/audioBuffers.js';
+
+
+describe('MPM Tartini', () => {
+
+    beforeEach(() => {
+
+    });
+
+    it('should...', () => {
+        console.log(buffers['C0_1024']);
+    });
+
+});
