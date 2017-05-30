05f3496ca45396df536d1e25aaa0832ea2800aa7
j-funk <jaboing@hotmail.com>
2017-05-29 22:27:41 -0400

    major progress on nsdf and autocorrelation functions

--- src/client/js/MPM_Tartini.babel.js
+++ src/client/js/MPM_Tartini.babel.js
@@ -13,17 +13,19 @@
 // k = (n + 1) / 2 ????
 // "size" = n + k
 
-import RFFT from '../../dependencies/rfft.js';
+import { RFFT } from 'fftw-js';
 
 const TWO_PI = 2 * Math.PI;
 const HALF_PI = Math.PI * 0.5;
 const RATE = 44100;
 const BUFFER_SIZE = 1024;
 const STEP_SIZE = BUFFER_SIZE / 2;
+const P_SIZE = BUFFER_SIZE + STEP_SIZE;
 const DB_FLOOR = -150;
 const DO_EQL_LOUD = true;
 
-let fft = new RFFT(BUFFER_SIZE);
+let baseFFT = new RFFT(BUFFER_SIZE);
+let acFFT = new RFFT(P_SIZE);
 
 let analysisData = [];
 let prevAnalysisData = [];
@@ -33,11 +35,13 @@ let dataHann = new Float32Array(BUFFER_SIZE);
 let dataFFT = new Float32Array(BUFFER_SIZE);
 let fftData1 = new Float32Array(STEP_SIZE);
 let fftData2 = new Float32Array(STEP_SIZE);
-let autoCorrData = new Float32Array(BUFFER_SIZE + STEP_SIZE);
-let nsdfData = [];
+let autoCorrTime = new Float32Array(BUFFER_SIZE + STEP_SIZE);
+let autoCorrFFT = new Float32Array(BUFFER_SIZE + STEP_SIZE);
+let nsdfData = new Float32Array(STEP_SIZE);   // TODO triple check this length
 
-    let hanningCoeff = [];  // array of floats size 'n'
+let hanningCoeff = [];  // array of floats size 'n'
 
+let sumOfSquares;
 let hanningScalar = 0;
 let maxIntensityDB = 0;
 
@@ -75,6 +79,7 @@ let linearToDB = function (element) {
 
 
 export default function calculateAnalysisData(inputBuffer) {
+    // TODO buffer size assertion or param, atm it is assume to be same as BUFFER_SIZE
     dataTime = inputBuffer;
 
     // TODO equal loudness bit
@@ -97,8 +102,10 @@ export default function calculateAnalysisData(inputBuffer) {
     /////////////////////////////
     // Do the channel data FFT //
     /////////////////////////////
-    dataFFT = fft.forward(dataHann);
 
+    // http://www.katjaas.nl/home/home.html  - interesting FFT walkthrough
+
+    dataFFT = baseFFT.forward(dataHann);
     let logSize = Math.log10(STEP_SIZE);
 
     //Adjust the coefficients, both real and imaginary parts by same amount
@@ -127,7 +134,43 @@ export default function calculateAnalysisData(inputBuffer) {
         fftData1[0] = DB_FLOOR;
     }
 
+    /////////////////
+    // Do the NSDF //
+    /////////////////
+    autoCorrTime.set(dataHann);
+    autoCorrFFT = acFFT.forward(autoCorrTime);  // TODO optimize this transfer so that autoCorrFFT is set inside the FFT
+
+    //calculate the (real*real + ima*imag) for each coefficient
+    //Note: The numbers are packed in half_complex form (refer fftw)
+    //ie. R[0], R[1], R[2], ... R[size/2], I[(size+1)/2+1], ... I[3], I[2], I[1]
+    for(let k = 1; k <  P_SIZE / 2; k++)
+    {
+        autoCorrFFT[k] = Math.pow(autoCorrFFT[k], 2) + Math.pow(autoCorrFFT[P_SIZE - k], 2);
+        autoCorrFFT[P_SIZE - k] = 0;
+    }
+    autoCorrFFT[0] = Math.pow(autoCorrFFT[0], 2);
+    autoCorrFFT[P_SIZE / 2] = Math.pow(autoCorrFFT[P_SIZE / 2], 2);
+
+    autoCorrTime = acFFT.inverse(autoCorrFFT);  // TODO optimize this transfer so that autoCorrTime is set inside the FFT
+
+    //extract the wanted elements out, and normalise --  see REPL https://repl.it/IWiH/19
+    // for(float * p1 = output, *p2 = autocorrTime + 1; p1 < output + k;)
+    // {
+    // *p1++ = *p2++ / fsize;
+    // }
+    for (let l = 0; l < STEP_SIZE; l++) {
+        nsdfData[l] = autoCorrTime[l + 1] / BUFFER_SIZE;
+    }
+    sumOfSquares = autoCorrTime[0] / BUFFER_SIZE;
+    let totalSumOfSquares = sumOfSquares * 2;
+
+    // This is the nsdf
+    for (let m = 0; m < STEP_SIZE; m++) {
+        totalSumOfSquares -= Math.pow(dataHann[BUFFER_SIZE - 1 - m], 2) + Math.pow(dataHann[m]);
+        if(totalSumOfSquares > 0) {
+            nsdfData[m] *= 2.0 / totalSumOfSquares;
+        } else {
+            nsdfData[m] = 0.0;
+        }
+    }
 }
-
-
-
